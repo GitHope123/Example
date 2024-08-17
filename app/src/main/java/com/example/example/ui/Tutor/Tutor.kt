d4a913a7f052d1d07475019e9c1276a6509bff27
GitHope123 <2201010253@undc.edu.pe>
2024-08-17 13:14:24 -0500

    git remote add origin https://github.com/GitHope123/APP_PROYECTO.git git branch -M main git push -u origin main

--- app/src/main/java/com/example/example/ui/Tutor/Tutor.kt
+++ app/src/main/java/com/example/example/ui/Tutor/Tutor.kt
@@ -1,12 +1,14 @@
 package com.example.example.ui.Tutor
 
-import androidx.fragment.app.viewModels
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.example.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class Tutor : Fragment() {
 
@@ -18,14 +20,22 @@ class Tutor : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         // TODO: Use the ViewModel
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.fragment_tutor, container, false)
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_tutor, container, false)
+
+        // Initialize the FloatingActionButton and set its click listener
+        val addButtonTutor: FloatingActionButton = view.findViewById(R.id.addButtomTutor)
+        addButtonTutor.setOnClickListener {
+            val intent = Intent(requireContext(), addTutor::class.java) // Use requireContext() for safety
+            startActivity(intent)
+        }
+
+        return view
     }
 }
