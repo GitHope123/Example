15743dd5aa7fe552418ca162fb4dea2a39e25651
Rodrigo101105 <2201010356@undc.edu.pe>
2024-08-16 23:21:47 -0500

    git remote add origin https://github.com/GitHope123/APP_PROYECTO.git git branch -M main git push -u origin main

--- app/src/main/java/com/example/example/ui/Estudiante/AddActivity.kt
+++ app/src/main/java/com/example/example/ui/Estudiante/AddActivity.kt
@@ -1,6 +1,9 @@
 package com.example.example.ui.Estudiante
 
+import android.app.DatePickerDialog
+import android.icu.util.Calendar
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,10 +15,18 @@ class AddActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_add)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val editTextFechaNacimiento: EditText = findViewById(R.id.editTextFechaNacimiento)
+        editTextFechaNacimiento.setOnClickListener {
+            val calendar = Calendar.getInstance()
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+            val datePickerDialog = DatePickerDialog(this, { _, selectedYear, selectedMonth, selectedDay ->
+                editTextFechaNacimiento.setText("$selectedDay/${selectedMonth + 1}/$selectedYear")
+            }, year, month, day)
+            datePickerDialog.show()
         }
+
     }
 }
