a0ea42ba9d643422f9ca4b2bfd406b201ab642d7
Rodrigo101105 <2201010356@undc.edu.pe>
2024-08-22 14:02:52 -0500

    git remote add origin https://github.com/GitHope123/APP_PROYECTO.git git branch -M main git push -u origin main

--- app/src/main/java/com/example/example/ui/Estudiante/Estudiante.kt
+++ app/src/main/java/com/example/example/ui/Estudiante/Estudiante.kt
@@ -2,12 +2,13 @@ package com.example.example.ui.Estudiante
 
 import java.io.Serializable
 
-data class Estudiante(
-    val id: String = "",
+    data class Estudiante(
+    val idEstudiante: String = "",
     val apellidos: String = "",
     val nombres: String = "",
     val celularApoderado: Long = 0,
     val dni: Long = 0,
     val grado: Int = 0,
-    val seccion: String = ""
+    val seccion: String = "",
+    val cantidadIncidencias:Int=0
 ):Serializable
