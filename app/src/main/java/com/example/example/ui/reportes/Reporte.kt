b002d8e562b9620adda18fefa85566813b634f31
GitHope123 <2201010253@undc.edu.pe>
2024-08-24 18:47:21 -0500

    git remote add origin https://github.com/GitHope123/APP_PROYECTO.git git branch -M main git push -u origin main

--- app/src/main/java/com/example/example/ui/reportes/Reporte.kt
+++ app/src/main/java/com/example/example/ui/reportes/Reporte.kt
@@ -4,12 +4,10 @@ import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.ImageButton
 import android.widget.Toast
 import androidx.fragment.app.Fragment
@@ -28,16 +26,32 @@ class Reporte : Fragment() {
         val view = inflater.inflate(R.layout.fragment_reporte, container, false)
 
         val button = view.findViewById<ImageButton>(R.id.buttonOpenLink)
-        button.setOnClickListener {
-            val url = "https://lookerstudio.google.com/reporting/55c2d7b2-0d63-4e02-895a-21ab9c7d018c" // Reemplaza con la URL que desees
+        val compartir = view.findViewById<ImageButton>(R.id.imageButtonCompartir)
 
-            // Copiar la URL al portapapeles
+        // URL que se utilizará
+        val url = "https://lookerstudio.google.com/reporting/55c2d7b2-0d63-4e02-895a-21ab9c7d018c" // Reemplaza con la URL que desees
+
+        // Configuración para copiar la URL al portapapeles
+        button.setOnClickListener {
             val clipboardManager = requireContext().getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
             val clipData = ClipData.newPlainText("Reporte URL", url)
             clipboardManager.setPrimaryClip(clipData)
 
             // Mostrar un mensaje de confirmación
             Toast.makeText(requireContext(), "URL copiada al portapapeles", Toast.LENGTH_SHORT).show()
+        }
+
+        // Configuración para compartir la URL a otras aplicaciones
+        compartir.setOnClickListener {
+            val sendIntent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, "Revisa este reporte: $url")
+                type = "text/plain"
+            }
+
+            // Crear el Intent Chooser para que el usuario elija la app
+            val shareIntent = Intent.createChooser(sendIntent, null)
+            startActivity(shareIntent)
         }
 
         return view
