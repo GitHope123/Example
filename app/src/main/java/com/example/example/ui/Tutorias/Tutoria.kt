876df7b699a0651ae5db351008f6f3775de2978b
Rodrigo101105 <2201010356@undc.edu.pe>
2024-08-23 21:53:31 -0500

    git remote add origin https://github.com/GitHope123/APP_PROYECTO.git git branch -M main git push -u origin main

--- app/src/main/java/com/example/example/ui/Tutorias/Tutoria.kt
+++ app/src/main/java/com/example/example/ui/Tutorias/Tutoria.kt
@@ -1,7 +1,7 @@
 package com.example.example.ui.Tutorias
-
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -20,10 +20,20 @@ class Tutoria : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         _binding= FragmentTutoriaBinding.inflate(inflater, container, false)
-        val fechaTutoria = resources.getStringArray(R.array.item_fecha_tutoria)
-        val arrayAdapter= ArrayAdapter(requireContext(), R.layout.list_tutoria,fechaTutoria)
-        binding.autoComplete.setAdapter(arrayAdapter)
+        resetAutoComplete()
         return binding.root
+    }
+    override fun onResume() {
+        super.onResume()
+        resetAutoComplete()
+    }
+
+    private fun resetAutoComplete() {
+        val fechaTutoria = resources.getStringArray(R.array.item_fecha_tutoria)
+        val arrayAdapter = ArrayAdapter(requireContext(), R.layout.list_tutoria, fechaTutoria)
+        binding.autoComplete.setAdapter(arrayAdapter)
+        binding.autoComplete.text.clear()
+        binding.autoComplete.setText("Todos",false)
     }
 
     override fun onDestroyView() {
