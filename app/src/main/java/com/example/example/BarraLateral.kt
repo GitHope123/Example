fb6ac53476900f3a2285d91c99be62ae2ff30989
hugoa <2201010253@undc.edu.pe>
2024-10-26 13:02:02 -0500

    git pull origin master

--- app/src/main/java/com/example/example/BarraLateral.kt
+++ app/src/main/java/com/example/example/BarraLateral.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
